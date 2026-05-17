86e7031ba01babf1bd6d880473ea8f03ed4abb47
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-05-17 15:47:38 +0200

    Create Product.cs

---
+++ Assignment3GUI_CS/Assignment3WinForm/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3WinForm
+{
+    public enum CategoryType
+    {
+        Food,
+        Electrics,
+        Tools
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+        public int Pieces { get; set; }
+        public CategoryType Category { get; set; }
+
+        public Product(string name, double weight, double price, int pieces, CategoryType category) // Constructor
+        {
+            Name = name;
+            Weight = weight;
+            Price = price;
+            Pieces = pieces;
+            Category = category;
+        }
+
+        public override string ToString() // Name of product and price
+        {
+            return $"{Name} - {Price}";
+        }
+    }
+}
