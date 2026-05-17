4e52ffd24bc9933fa37e83289f2e350cff48bfe0
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-05-17 19:10:55 +0200

    Create Producer.cs

---
+++ Assignment3GUI_CS/Assignment3WinForm/Producer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3WinForm
+{
+    public class Producer
+    {
+        private Storage storage;
+        private Product[] products;
+
+        private bool running = true;
+
+        private Random random = new Random();
+
+        public Producer(Storage storage, Product[] products)
+        {
+            this.storage = storage;
+            this.products = products;
+        }
+
+        public void Stop()
+        {
+            running = false;
+        }
+
+        public void Run()
+        {
+            while (running)
+            {
+                int index = random.Next(products.Length); // Randomly select a product from the array
+
+                Product p = products[index]; // Get the selected product
+
+                storage.Produce(p); // Add the product to the storage
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
