0dd9f48255c9315c0bde16763219d71770917ef9
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-05-17 19:11:54 +0200

    Create Consumer.cs

---
+++ Assignment3GUI_CS/Assignment3WinForm/Consumer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3WinForm
+{
+    public class Consumer
+    {
+        private Storage storage;
+
+        private bool running = true;
+
+        public Consumer(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public void Stop()
+        {
+            running = false;
+        }
+
+        public void Run()
+        {
+            while (running)
+            {
+                Product p = storage.Consume(); // Retrieve a product from the storage
+
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
